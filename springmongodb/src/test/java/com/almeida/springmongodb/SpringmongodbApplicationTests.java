8c45ff1f0604a995e8424902955d4428c4cded4a
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-11-18 18:16:09 -0300

    feat: created project springmongo

---
+++ springmongodb/src/test/java/com/almeida/springmongodb/SpringmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.almeida.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
