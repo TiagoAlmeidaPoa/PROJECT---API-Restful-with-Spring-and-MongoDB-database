4f46163cc53f4041fea00e54c73ff4ec253c0ad0
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-11-28 15:30:05 -0300

    feat: add annotation @Builder in UserEntity class

--- springmongodb/src/main/java/com/almeida/springmongodb/domain/UserEntity.java
+++ springmongodb/src/main/java/com/almeida/springmongodb/domain/UserEntity.java
@@ -1,6 +1,7 @@
 package com.almeida.springmongodb.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,6 +11,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 public class UserEntity implements Serializable {
     private String id;
     private String name;
