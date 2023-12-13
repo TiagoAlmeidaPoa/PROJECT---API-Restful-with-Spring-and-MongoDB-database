84d1fdda8286d4bfa4df27875e7c06b5e1a7c48d
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-12-13 20:36:32 -0300

    test: created test for main class

--- springmongodb/src/test/java/com/almeida/springmongodb/SpringmongodbApplicationTests.java
+++ springmongodb/src/test/java/com/almeida/springmongodb/SpringmongodbApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SpringmongodbApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		SpringmongodbApplication.main(new String[] {});
 	}
 
 }
