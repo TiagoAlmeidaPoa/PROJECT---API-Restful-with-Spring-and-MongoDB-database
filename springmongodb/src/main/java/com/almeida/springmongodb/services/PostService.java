64809f7fd889d1d15cc97c8c4aa213a319f84164
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-12-12 21:30:21 -0300

    feat: creating query with multiple criteria

--- springmongodb/src/main/java/com/almeida/springmongodb/services/PostService.java
+++ springmongodb/src/main/java/com/almeida/springmongodb/services/PostService.java
@@ -6,10 +6,13 @@ import com.almeida.springmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
+
+    private Integer dia = 24 * 60 * 60 * 1000;
 
     @Autowired
     private PostRepository repository;
@@ -21,6 +24,11 @@ public class PostService {
 
     public List<Post> findByTitle(String title) {
         return repository.searchTitle(title);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + dia);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 
 }
