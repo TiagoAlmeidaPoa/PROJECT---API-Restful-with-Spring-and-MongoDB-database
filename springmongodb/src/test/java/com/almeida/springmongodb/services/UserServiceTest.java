01c1501a174229d0fbfcd29dc22999e79cc6270f
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-12-15 19:07:48 -0300

    Test: created UserServiceTest test class

---
+++ springmongodb/src/test/java/com/almeida/springmongodb/services/UserServiceTest.java
@@ -0,0 +1,35 @@
+package com.almeida.springmongodb.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void created() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
