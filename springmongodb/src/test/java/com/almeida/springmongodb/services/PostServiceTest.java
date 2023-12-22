d4bf6645078cef8635a4fc302efa5446329b0629
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-12-22 20:05:54 -0300

    test: created structure for PostServiceTest service class

---
+++ springmongodb/src/test/java/com/almeida/springmongodb/services/PostServiceTest.java
@@ -0,0 +1,25 @@
+package com.almeida.springmongodb.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByTitle() {
+    }
+
+    @Test
+    void fullSearch() {
+    }
+}
