622cd2a522362e5dba96563db79b82f1b95165bc
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-12-26 17:57:16 -0300

    test: created UserResourceTest test class

---
+++ springmongodb/src/test/java/com/almeida/springmongodb/resources/UserResourceTest.java
@@ -0,0 +1,37 @@
+package com.almeida.springmongodb.resources;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserResourceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void created() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findPosts() {
+    }
+}
